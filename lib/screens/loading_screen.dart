e1c219b313b626c740638a717ea0faff1b40e552
gusmac <gusmac@gmail.com>
2020-04-26 19:15:34 +0800

    added spinkit package and route to loadingscreen

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -8,6 +9,13 @@ class LoadingScreen extends StatefulWidget {
 class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.blueGrey,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
